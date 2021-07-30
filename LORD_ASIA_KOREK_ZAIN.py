1ae354b75c2da9793e6f178a69cf06685d4d1bbc
HaMa LoRdy EzRaiLy <63779958+hamalord4444@users.noreply.github.com>
2021-07-31 01:05:23 +0300

    Add files via upload

---
+++ LORD_ASIA_KOREK_ZAIN.py
@@ -0,0 +1,114 @@
+import os
+import random
+
+
+os.system("clear")
+
+lghl = """
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$HAMA_LORD$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+"""
+
+
+def asia():
+	op = open("asia.txt","w")
+	os.system("clear")
+	print(lghl)
+	e = "133"
+	for x in range(9999999999):
+		c = "1234567890"
+		x1 = random.choice(c)
+		x2 = random.choice(c)
+		x3 = random.choice(c)
+		x4 = random.choice(c)
+		x5 = random.choice(c)
+		x6 = random.choice(c)
+		x7 = random.choice(c)
+		x8 = random.choice(c)
+		x9 = random.choice(c)
+		x10 = random.choice(c)
+		x11 = random.choice(c)
+		x12 = random.choice(c)
+		x13 = random.choice(c)
+		x14 = random.choice(c)
+		b = "*"+e+"*"+x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11+x12+x13+x14+"#"
+		print(b)
+		op.write(b+"\n")
+
+def korek():
+	op = open("korak.txt","w")
+	os.system("clear")
+	print(lghl)
+	e = "221"
+	for x in range(9999999999):
+		c = "1234567890"
+		x1 = random.choice(c)
+		x2 = random.choice(c)
+		x3 = random.choice(c)
+		x4 = random.choice(c)
+		x5 = random.choice(c)
+		x6 = random.choice(c)
+		x7 = random.choice(c)
+		x8 = random.choice(c)
+		x9 = random.choice(c)
+		x10 = random.choice(c)
+		x11 = random.choice(c)
+		x12 = random.choice(c)
+		x13 = random.choice(c)
+		x14 = random.choice(c)
+		b = "*"+e+"*"+x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11+x12+x13+x14+"#"
+		print(b)
+		op.write(b+"\n")
+	
+def zain():
+	op = open("zain.txt","w")
+	os.system("clear")
+	print(lghl)
+	e = "101"
+	for x in range(9999999999):
+		c = "1234567890"
+		x1 = random.choice(c)
+		x2 = random.choice(c)
+		x3 = random.choice(c)
+		x4 = random.choice(c)
+		x5 = random.choice(c)
+		x6 = random.choice(c)
+		x7 = random.choice(c)
+		x8 = random.choice(c)
+		x9 = random.choice(c)
+		x10 = random.choice(c)
+		x11 = random.choice(c)
+		x12 = random.choice(c)
+		x13 = random.choice(c)
+		x14 = random.choice(c)
+		b = "*"+e+"*"+x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11+x12+x13+x14+"#"
+		print(b)
+		op.write(b+"\n")
+		
+def i():
+		print(lghl)
+		print("=======================")
+		print("KARTY ASIA W KOREK W ZAIN test him")
+		print("99000 CARD")
+		print("[1] Asia")
+		print("[2] Korek")
+		print("[3] Zain")
+		print("=======================")
+		option = int(input("CHOOSE ONES: "))
+		if option==1:
+			os.system("clear")
+			asia()
+		elif option==2:
+			os.system("clear")
+			korek()
+		elif option==3:
+		    os.system("clear")
+		    zain()
+i()
